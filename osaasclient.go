f35e1e6b3f6f2479af63645f4ad670d6f15044e1
Ludvig Noring <ludvig.noring@gmail.com>
2024-11-19 09:57:33 +0100

    active service before creating

--- osaasclient.go
+++ osaasclient.go
@@ -31,10 +31,15 @@ func (e UnauthorizedError) Error() string {
 }
 
 func GetService(ctx *Context, serviceId string) (*Service, error) {
+	err := ctx.ActivateService(serviceId)
+	if err != nil {
+		return nil, err
+	}
+
 	serviceURL := fmt.Sprintf("https://catalog.svc.%s.osaas.io/mysubscriptions", ctx.GetEnvironment())
 
 	var services []Service
-	err := createFetch(serviceURL, "GET", nil, &services,
+	err = createFetch(serviceURL, "GET", nil, &services,
 		Auth{"x-pat-jwt", fmt.Sprintf("Bearer %s", ctx.GetPersonalAccessToken())})
 	if err != nil {
 		return nil, err
@@ -50,6 +55,11 @@ func GetService(ctx *Context, serviceId string) (*Service, error) {
 
 func CreateInstance(ctx *Context, serviceId string, token string,
 	body map[string]interface{}) (map[string]interface{}, error) {
+	err := ctx.ActivateService(serviceId)
+	if err != nil {
+		return nil, err
+	}
+
 	service, err := GetService(ctx, serviceId)
 	if err != nil {
 		return nil, err
